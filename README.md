d434f7d8d1e066f1df8a0a0cfd3486694ca92aed
Valentine <valentinemaillard1@gmail.com>
2020-07-02 16:08:39 -0400

    array sum solution

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 This repo focuses on practicing Algorithms & Data Structures
 <br/>
-Browse through the files to check out my solutions in JavaScript only...Sorry for you if you don't use JS.# HackerRank
+Browse through the files to check out my solutions in JavaScript only...Sorry for you if you don't use JS.
---
+++ array-sum.js
@@ -0,0 +1,27 @@
+// Given an array of integers, find the sum of its elements.
+
+// For example, if the array, [1, 2, 3], so return 6.
+
+// Function Description
+// Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+// Print the sum of the array's elements as a single integer.
+// Sample Input
+// 1 2 3 4 10 11
+// Sample Output
+// 31
+
+function simpleArraySum(ar) {
+    /*
+     * Write your code here.
+     */
+    let num = 0;
+
+    for(let i of ar){
+        num += i
+    }
+    return num;
+}
+console.log(simpleArraySum([1, 2, 3], 6));
+console.log(simpleArraySum([1, 3, 5, 2, 2], 13));
+console.log(simpleArraySum([], 0));
